0d1c8b61a2b604a7eefc4ff22075d0250034b98c
moerwald <andreas@mewald.com>
2019-10-19 18:21:38 +0200

    Added tests for ForEach-GitRepository

--- src/tests/Public/Iterating/Foreach-GitRepository.tests.ps1
+++ src/tests/Public/Iterating/Foreach-GitRepository.tests.ps1
@@ -16,7 +16,30 @@ Describe "Tests to check if repository iterating works" {
         Pop-Location
     }
 
-    It "Index file contains three GIT repos" {
+    It "All three repositories shall be iterated through"{
+        $repos = [System.Collections.ArrayList]( "repo_1", "repo_2", "repo_3")
+        $script:ctnr = 0
+        ForEach-GitRepository -Callback { 
+            $script:ctnr++ 
+            $repos.Remove($_.Name)
+        }
 
+        # Check if all repos were iterated
+        $script:ctnr | Should -Be 3
+        $repos.Count | Should -Be 0
+    }
+
+    It "Exclude repo_2 from iteration"{
+        $repos = [System.Collections.ArrayList]( "repo_1", "repo_2", "repo_3")
+        $script:ctnr = 0
+        ForEach-GitRepository -Callback { 
+            $script:ctnr++ 
+            $repos.Remove($_.Name)
+        } -Predicate { $_.Name -ne "repo_2"}
+
+        # Check if all repos were iterated
+        $script:ctnr | Should -Be 2
+        $repos.Count | Should -Be 1
+        "repo_2" | Should -BeIn $repos
     }
 }

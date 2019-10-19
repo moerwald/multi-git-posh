9176d6f6affa5e5d687685a4e61aaac48ce4f02b
moerwald <andreas@mewald.com>
2019-10-19 15:25:56 +0200

    Added new test case to check number of repos in index file.

--- src/tests/Public/Index/New-GitRepositoryIndex.tests.ps1
+++ src/tests/Public/Index/New-GitRepositoryIndex.tests.ps1
@@ -32,6 +32,13 @@ Describe "Tests for index file descripton" {
             New-GitRepositoryIndex 
 
             Test-Path "./.index_multi_git_posh/index_git_repos.json" | Should -BeTrue
+        }
+
+        It "Index file contains three GIT repos" {
+            New-GitRepositoryIndex 
+
+            $index = Get-Content "./.index_multi_git_posh/index_git_repos.json" | ConvertFrom-Json
+            $index.Repositories.Count | Should -Be 3
 
         }
 }

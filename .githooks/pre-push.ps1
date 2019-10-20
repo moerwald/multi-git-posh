5c4afb01e14e185d1ec1cab0fff3a55f59081b1b
moerwald <andreas@mewald.com>
2019-10-20 08:25:23 +0200

    Fixed typo

--- .githooks/pre-push.ps1
+++ .githooks/pre-push.ps1
@@ -5,7 +5,7 @@ if ($filesToBePushed | Where-Object { $_ -match ".*.ps1" }) {
     # Call build script and check result code
     try {
         Push-Location
-        Set-Location "$PScriptRoot/../src/tests"
+        Set-Location "$PSScriptRoot/../src/tests"
         if (Invoke-Pester -PassThru | Where-Object { $_.TestResult.Passed -eq $false }) {
             throw "Pester tests failed, won't push code to remote"
         }

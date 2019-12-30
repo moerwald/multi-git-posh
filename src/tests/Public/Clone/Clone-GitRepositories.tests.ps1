baaf38f4b29e35b84952e8641627d4f297f1aacf
moerwald <andreas.mewald@gmail.com>
2019-12-30 16:09:33 +0100

    - Added tests

---
+++ src/tests/Public/Clone/Clone-GitRepositories.tests.ps1
@@ -0,0 +1,45 @@
+
+Remove-Module MultiGitPosh -ErrorAction SilentlyContinue
+Import-Module "$PSScriptRoot/../../../module/MultiGitPosh/MultiGitPosh.psd1" 
+
+Describe "Tests to check if git status functionallity works" {
+
+    $cloneDirectory = Join-Path $env:TEMP ([System.Guid]::NewGuid())
+    $indexFilePath = Join-Path $cloneDirectory "./.index_multi_git_posh/index_git_repos.json"
+
+    BeforeEach {
+        Push-Location
+
+        Clone-GitRepositories -GitIndexfile ".\index.json" -DirectoryToCloneTo $cloneDirectory
+        Set-Location $cloneDirectory
+    }
+
+    AfterEach {
+        Pop-Location
+        Remove-Item -Path $cloneDirectory -Recurse -Force
+    }
+
+    Context "Clone repos from index file" {
+
+        It "Number of cloned repos is correct" {
+            function NRepositoriesExist {
+                param($NrOfRepos)
+
+                (Get-ChildItem).Count | Should -Be $NrOfRepos
+            }
+
+            NRepositoriesExist -NrOfRepos 2
+        }
+
+        It "Index file was copied to clone directory" {
+            (Get-ChildItem $indexFilePath).Count | Should -Be 1
+        }
+
+        It "Path attribute was set in index file " {
+            $gitRepoInfo = Get-Content -Path $indexFilePath | ConvertFrom-Json -Depth 10
+            foreach( $repo in $gitRepoInfo.Repositories) {
+                $repo.Path | Should -Not -BeNullOrEmpty
+            }
+        }
+    }
+}
